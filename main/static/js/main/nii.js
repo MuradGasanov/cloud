848115116500eff122c2c53f1fa73b6c316f4d2d
Murad <gmn1791@mail.ru>
2014-04-18 09:16:37 +0400

    Исправление в css

--- main/static/js/main/nii.js
+++ main/static/js/main/nii.js
@@ -8,26 +8,72 @@ var Nii = (function () {
                 $(window).trigger("show_tree");
             });
 
+            var validator_option = {
+                rules: {
+                    required: function (input) {
+                        if (input.is("[required]")) {
+                            input.val($.trim(input.val())); //удалить обертывающиепробелы
+                            return input.val() !== "";
+                        } else return true;
+                    }
+                },
+                messages: {
+                    required: "Поле не может быть пустым"
+                }
+            };
+
+            var MESSAGE = {
+                error: "Ошибка: ",
+                wait: "Загрузка...",
+                done: "Выполнено"
+            };
+
+             var PROJECT_TREE_BASE_URL = "project_tree/",
+                 NII_BASE_URL = "nii";
+
             $("#nii").kendoComboBox({
-                data: new kendo.dataSource({
+                dataSource: {
                     transport: {
                         read: function(options) {
                             $.ajax({
                                 url: PROJECT_TREE_BASE_URL + "nii/read/",
                                 dataType: "json",
                                 type: "POST",
-                                data: { },
+                                data: {},
                                 success: function (result) {
-                                    options.success.(result);
+                                    options.success(result);
                                 },
                                 error: function (result) {
+                                    noti({title: MESSAGE.error + result.status, message: result.statusText}, "error");
                                     options.error(result);
                                 }
                             });
                         }
                     }
-                })
+                },
+                dataTextField: "name",
+                dataValueField: "id",
+                filter: "contains",
+                change: function(e) {
+                    var that = this;
+                    var val = that.value();
+                    console.log(val);
+                }
             });
+
+            var direction_model = kendo.observable({
+                is_edit: false,
+                o: {
+                    id: 0,
+                    university: null,
+                    university_list: [],
+                    name: ""
+                }
+            });
+            var $change_direction = $("#change_direction");
+            kendo.bind($change_direction, direction_model);
+            var direction_validator = $change_direction.kendoValidator(validator_option).data("kendoValidator");
+            var $change_direction_window = $("#change_direction_window");
         }
     }
 })();
